dc9f43798ebf9d27399b9105ce6cf7f5b77a98b6
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-09-21 14:15:22 +0300

    Add files via upload

---
+++ 3 семинар/3-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int is_even(int num) {
+	if (num % 2 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main() {
+	printf("%i\n", is_even(90));
+	printf("%i\n", is_even(91));
+}
