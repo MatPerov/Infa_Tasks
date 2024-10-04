96d8b0a0c59ede2768313d2db086202aeba7263f
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-10-04 11:06:59 +0300

    Update 6-7.c

--- 6 семинар/6-7.c
+++ 6 семинар/6-7.c
@@ -8,6 +8,6 @@ int exchange(int* pa, int b) {
 int main()
 {
     int a = 10;
-    printf("%i\n", exchange(&a, 20));  // Напечатает 10
-    printf("%i\n", a);                 // Напечатает 20
+    printf("%i\n", exchange(&a, 20));  
+    printf("%i\n", a);                 
 }
