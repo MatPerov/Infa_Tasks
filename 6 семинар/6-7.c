f11e0bcad889493a93e1c7d9bf76821ebd7e041b
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-10-04 11:06:28 +0300

    Add files via upload

---
+++ 6 семинар/6-7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int exchange(int* pa, int b) {
+    int num = *pa;
+    *pa = 20;
+    return num;
+}
+
+int main()
+{
+    int a = 10;
+    printf("%i\n", exchange(&a, 20));  // Напечатает 10
+    printf("%i\n", a);                 // Напечатает 20
+}
