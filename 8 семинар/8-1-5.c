4235e5b9ce01adc1a54f7840a2024e580b2e4cac
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-10-12 17:06:39 +0300

    Add files via upload

---
+++ 8 семинар/8-1-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct book
+{
+    char title[50];
+    int pages;
+    float price;
+};
+typedef struct book Book;
+
+void print_book(struct book b)
+{
+    printf("Book: %s, Pages: %i, Price: %g\n", b.title, b.pages, b.price);
+}
+
+int main() {
+    Book** p = (Book**)malloc(sizeof(Book*));
+    Book a = { {"Don Quixote"},{1000},{750.0} };
+    *p = &a;
+    print_book(**p);
+}
