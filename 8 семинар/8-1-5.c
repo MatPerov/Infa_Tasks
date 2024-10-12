85ba501aa7bc2ed0ea4607c99a44389a3dcd154a
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-10-12 18:21:57 +0300

    Update 8-1-5.c

--- 8 семинар/8-1-5.c
+++ 8 семинар/8-1-5.c
@@ -17,6 +17,8 @@ void print_book(struct book b)
 
 int main() {
     Book** p = (Book**)malloc(sizeof(Book*));
+    if (p == NULL)
+        printf("Error. Can't allocate memory!\n");
     Book a = { {"Don Quixote"},{1000},{750.0} };
     *p = &a;
     print_book(**p);
