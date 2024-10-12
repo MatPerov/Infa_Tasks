503b776670e2f9aca9f8acae44382f32af6dbafd
MatPerov <145116122+MatPerov@users.noreply.github.com>
2024-10-12 18:31:37 +0300

    Add files via upload

---
+++ 8 семинар/8-1-8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct book
+{
+    char* title;
+    int pages;
+    float price;
+};
+typedef struct book Book;
+
+void print_book(struct book b)
+{
+    printf("Book: %s, Pages: %i, Price: %g\n", b.title, b.pages, b.price);
+}
+
+int main() {
+    char* pstr = (char*)malloc(sizeof(char));
+    if (pstr == NULL)
+        printf("Error. Can't allocate memory!\n");
+    Book* p = (Book*)malloc(sizeof(Book));
+    if (p == NULL)
+        printf("Error. Can't allocate memory!\n");
+    char** pstr_heap = (char**)malloc(sizeof(char*));
+    if (pstr_heap == NULL)
+        printf("Error. Can't allocate memory!\n");
+    *pstr_heap = &*pstr;
+    strcpy(pstr, "Don Quixote");
+    p->title = *pstr_heap;
+    p->pages = 1000;
+    p->price = 750.0;
+    print_book(*p);
+}
