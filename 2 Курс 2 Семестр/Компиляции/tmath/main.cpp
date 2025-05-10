7c4b35f9db5ea84bc769f79f0a328efde4ba77f8
MatPerov <145116122+MatPerov@users.noreply.github.com>
2025-05-10 12:50:58 +0300

    Add files via upload

---
+++ 2 Курс 2 Семестр/Компиляции/tmath/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "tmath.hpp"
+
+int main()
+{
+    std::cout << sum(10, 5) << std::endl;
+    std::cout << sum(0.5, 1.2) << std::endl;
+    std::cout << mult(10, 5) << std::endl;
+    std::cout << mult(0.5, 1.2) << std::endl;
+
+}
---
+++ 2 Курс 2 Семестр/Компиляции/tmath/tmath.hpp
@@ -0,0 +1,6 @@
+#pragma once
+template<typename T>
+T sum(T x, T y);
+
+template<typename T>
+T mult(T x, T y);
